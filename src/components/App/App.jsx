76c6bf90c395af195982ec10670e7e888ce3d08f
ebdsix66 <ebdsix66@doubletapp.ru>
2020-01-21 01:00:09 +0500

    modal starter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,31 @@
-import {h, Component} from 'preact'
+import { h, Component } from 'preact'
+import { useState } from 'preact/hooks';
 import Movie from "../Movie/Movie";
+import { createPortal } from 'preact/compat';
 import './App.css'
 
 export default class App extends Component {
     render() {
+        const container = document.getElementById('modals');
+        const [modalIsOpen, setModalIsOpen] = useState(false);
+        const [currentModalContent, setCurrentModalContent] = useState({});
+        const { time, film, seats } = currentModalContent;
+
+        const onModalClick = (modalContent) => { 
+            setCurrentModalContent(modalContent);
+            setModalIsOpen(true) 
+        };
+
         return (
             <div>
+                {modalIsOpen && createPortal(
+                    <div className="modal">
+                        <div className="modal__container">
+                            {film} starts at {time}
+                        </div>
+                        <div>seats {seats}</div>
+                    </div>,
+                    container)}
                 <h1 className="header">
                     Пятница, 22 ноября
                 </h1>
@@ -13,6 +33,8 @@ export default class App extends Component {
                     <li className="movie-list__entry">
                         <Movie
                             poster={"https://sun9-20.userapi.com/c856032/v856032322/177f4a/GbtPOQBAsLw.jpg"}
+                            onScheduleClick={onModalClick}
+                            filmName="sosat"
                             schedule={[
                                 '10:00',
                                 '12:00'
@@ -22,6 +44,8 @@ export default class App extends Component {
                     <li className="movie-list__entry">
                         <Movie
                             poster={"https://mcswebsites.blob.core.windows.net/1013/Event_7123/portrait_medium/plfykyRw.jpg"}
+                            onScheduleClick={onModalClick}
+                            filmName="suda"
                             schedule={[
                                 '10:00',
                                 '12:00',
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,17 +1,25 @@
-import {h, Component, Fragment} from 'preact'
+import { h, Component, Fragment } from 'preact'
 
 export default class Movie extends Component {
     render() {
-        const { schedule, poster } = this.props;
+        const { schedule, poster, onScheduleClick, filmName } = this.props;
         return (
             <Fragment>
                 <img className="poster"
-                     src={poster}
-                     alt="Постер фильма"/>
+                    src={poster}
+                    alt="Постер фильма" />
                 <ul className="schedule">
                     {schedule.map(entry => (
                         <li className="schedule__time">
-                            <button className="schedule__time-button">{entry}</button>
+                            <button
+                                className="schedule__time-button"
+                                onClick={() => {onScheduleClick({
+                                    time: entry,
+                                    film: filmName,
+                                    seats: 0
+                                })}}>
+                                {entry}
+                            </button>
                         </li>
                     ))}
                 </ul>
